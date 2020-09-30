78b9a42d727e47de2aab3f49fb0141ac72cced95
Lafie-rage <corentin.destrez62@gmail.com>
2020-09-30 15:24:16 +0200

    Part 5 done

--- TP2/test_mutex1.c
+++ TP2/test_mutex1.c
@@ -10,26 +10,43 @@
 
 #define NB_THREADS 10
 
+pthread_mutex_t mutex1;
+
 void *thread_treatement (void * arg);
 
 int main(void) {
   pthread_t threads[NB_THREADS];
-  int i = 0;
+  char i = 0;
   void *retVal_threads[NB_THREADS];
 
-  for (; i < NB_THREADS; i++) {
-    pthread_create(threads[i], NULL, thread_treatement, (void *)i);
+  for (i = 0; i < NB_THREADS; i++) {
+    pthread_create(&(threads[i]), NULL, thread_treatement, (void *)i);
   }
 
   for (i = 0; i < NB_THREADS; i++) {
-    pthread_join(threads[i], retVal_threads[i]);
+    pthread_join(threads[i], &retVal_threads[i]);
   }
 
   return EXIT_SUCCESS;
 }
 
 void *thread_treatement (void *arg) {
-  int threadNb = (int)arg;
-  
+  int i = 0;
+  int j;
+  int result = 0;
+  int nb_iter = rand()%10;
+  int threadNb = (int)(arg);
+
+  printf("Thread %d fera %d tour de boucle\n", threadNb, nb_iter);
+
+  for (; i < nb_iter; i++) {
+    pthread_mutex_lock(&mutex1);
+    printf("Debut calcul thread %d\n", threadNb);
+    for (j = 0; j < 10; j++) {
+      result += 30 * rand()%100;
+    }
+    printf("Fin calcul thread %d\n", threadNb);
+    pthread_mutex_unlock(&mutex1);
+  }
   pthread_exit(EXIT_SUCCESS);
 }
