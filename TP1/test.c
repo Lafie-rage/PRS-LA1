e024e8aa96a97fa3423d9779ac66699acda86757
Lafie-rage <corentin.destrez62@gmail.com>
2020-09-30 13:48:30 +0200

    Removing useless source and executables

--- TP1/test.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <sched.h>
-
-int main (void) {
-  printf("SCHED_FIFO : %d\n", SCHED_FIFO);
-  printf("SCHED_RR : %d\n", SCHED_RR);
-  printf("SCHED_OTHER : %d\n", SCHED_OTHER);
-}
